a17689211d80b1909456734fc93006e4484659b5
brunobme <marqueisbruno@gmail.com>
2019-07-03 15:58:48 +0100

    wards&punis feature to add and remove todos

--- apps/wards&punis/js/main.js
+++ apps/wards&punis/js/main.js
@@ -1,15 +1,35 @@
 var app = new Vue({
 	el: '#app',
 	data: {
-		title: "Wards & Punis",
+		title: 'Wards & Punis',
+		todo: {
+			what: '',
+			due_date:'',
+			details: '',
+			create_date:'',
+		},
+		to_dos: [
+			{
+				what: 'Finish Vue HandsOn',
+				due_date:'03/07/2019',			
+				details: 'Finish the Vue HandsOn app and share on github',
+				create_date: new Date(Date.now()).toLocaleString()
+			}
+		],
 		bool: false,
 	},
 	methods:{
-		toggle() {
-			var bool = this.bool;
-			this.bool = !bool;
-			return bool
-			// alert(`Success: ${this.randN}`)
+		add() {
+			let { what, due_date, details } = this.todo
+			this.to_dos.push({
+				what, 
+				due_date, 
+				details, 
+				create_date: new Date(Date.now()).toLocaleString()
+			})
+		},
+		remove(index) {
+			this.to_dos.splice(index, 1)
 		}
 	}
 })
